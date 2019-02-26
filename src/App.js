1d1af74f82a94fd307244b3cf7fa39faff08f5d8
Sasu <sasuolander@yahoo.com>
2019-02-26 17:59:05 +0200

    m

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Axios from 'axios';
 import {Footer} from './component/elements/Footer'
 import {Header} from './component/elements/Header'
 import {csvParse} from "d3-dsv";
-import DefaultDiagramTest2 from "./component/resultdiagram/DefaultDiagramTest2";
+//import DefaultDiagramTest2 from "./component/resultdiagram/DefaultDiagramTest2";
 //global namespace
 const API_URL = "https://gist.githubusercontent.com/sasuolander/54feb87d8a2ecf03e32e5e03d61aaf2a/raw/30ee2a2e22dbeac645ae55b0df7aa2bdd59307c7/data.csv";
 export default class App extends Component {
