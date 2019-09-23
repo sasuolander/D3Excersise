b42f9397deb06932e540cafb45f467af39d90d95
Sasu <sasuolander@yahoo.com>
2019-09-23 17:19:05 +0300

    redux devtool bug fix

--- src/component/redux/store.js
+++ src/component/redux/store.js
@@ -1,19 +1,20 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducer/mainReducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 
 const initialState = {};
 
 const middleware = [thunk];
 
+
 const store = createStore(
     rootReducer,
     initialState,
-    composeWithDevTools(
+    compose(
         applyMiddleware(...middleware),
-        
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose
     )
 );
 
